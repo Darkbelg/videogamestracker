353d45ec042d311ecbceac9d993efc0a9323eb71
Darkbelg <stijn.sagaert@outlook.com>
2021-02-10 18:07:45 +0100

    Episode 12 Refactoring to Blade Components

---
+++ resources/views/components/game-card-skeleton.blade.php
@@ -0,0 +1,11 @@
+<div class="game mt-8">
+    <div class="relative inline-block">
+        <div class="bg-gray-800 w-44 h-56"></div>
+    </div>
+    <div class="block text-transparent text-lg rounded bg-gray-700 text-base font-semibold leading-tight mt-4">
+        Title goes here
+    </div>
+    <div class="text-transparent rounded bg-gray-700 inline-block mt-3">
+        PS4,PC,Switch
+    </div>
+</div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -142,35 +142,7 @@
             
             @if (isset($game['similarGames']))
             @foreach ($game['similarGames'] as $game)
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    @if (isset($game['coverImageUrl']))
-                    <a href="{{ route('games.show',$game['slug'])}}">
-                        <img src="{{ $game['coverImageUrl'] }}" alt="game cover"
-                            class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    @endif
-
-
-                    @if (isset($game['rating']))
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
-                        style="right:-20px; bottom:-20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            {{ round($game['rating']). '%'}}</div>
-                    </div>
-                    @endif
-
-                </div>
-                <a href="{{ route('games.show',$game['slug'])}}"
-                    class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
-                    {{ $game['name'] }}
-                </a>
-                <div class="text-gray-400 mt-1">
-                    @if (isset($game['platforms']))
-                    {{$game['platforms']}}
-                    @endif
-                </div>
-            </div>
+            <x-game-card :game="$game" />
             @endforeach
             @else
             <div>
